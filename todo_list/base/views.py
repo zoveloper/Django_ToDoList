494802fe4ce61ca01f30c85d58a39201db72488c
Martin Cho <zoveloper@gmail.com>
2021-04-29 12:03:28 -0400

    added login/out fuctions

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -4,8 +4,17 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
 
 from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user =True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks') 
 
 
 class TaskList(ListView):
